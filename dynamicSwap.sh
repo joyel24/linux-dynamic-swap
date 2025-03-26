8ed76b484a896869bf7e176014b7fb8cde341947
joyel24 <joel.martin.2104@gmail.com>
2025-03-26 02:44:19 +0100

    clarification variables + add comments dynamicSwap.sh

--- dynamicSwap.sh
+++ dynamicSwap.sh
@@ -1,29 +1,31 @@
-test=`free --giga | awk 'NR==2 {print $4}'`
-max=7
-swapTrigger=20
-swapTriggerDown=4
+minFreeRAM=7                #Miniimum free GB RAM before enabling swap
+swapTriggerUp=20            #Max swap GB used before enabling secondary swap partition
+swapTriggerDown=4           #Do not disable 1st swap partition (nvme0n1p3 in my case) if swap GB used is > than this variable
+swap1=/dev/nvme0n1p3        #Prioritqry swap file or partition (Faster one, nvme ssd in my case)
+swap2=/dev/sda              #Secondary swap (slower, SD card swap partition in my case)
 
 while true
 do
         sleep 0.5
-        test=`free --giga | awk 'NR==2 {print $4}'`
-        if (($test < $max))
+        
+        freeRAM=`free --giga | awk 'NR==2 {print $4}'` #Get free RAM memory in GB
+        if (($freeRAM < $minFreeRAM))
         then
-                swapon /dev/nvme0n1p3
+                swapon $swap1
         fi
 
-        test2=`free --giga | awk 'NR==3 {print $3}'`
+        usedSWAP=`free --giga | awk 'NR==3 {print $3}'` #Get swap usage in GB
 
-        if (($test2 > $swapTrigger))
+        if (($usedSWAP > $swapTriggerUp))
         then
-                swapon /dev/sda
+                swapon $swap2
         else
-                swapoff /dev/sda
+                swapoff $swap2
         fi
 
-        if (($test > $max)) && (($test2 < $swapTriggerDown))
+        if (($freeRAM > $minFreeRAM)) && (($usedSWAP < $swapTriggerDown))
         then
-                swapoff /dev/nvme0n1p3
+                swapoff $swap1
         fi
 
 done
