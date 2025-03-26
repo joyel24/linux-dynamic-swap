e79682d4e882f93e2dc2cdcd15c836ae1cfab8a6
joyel24 <joel.martin.2104@gmail.com>
2025-03-26 02:24:58 +0100

    remove tests echos

--- dynamicSwap.sh
+++ dynamicSwap.sh
@@ -1,7 +1,7 @@
 test=`free --giga | awk 'NR==2 {print $4}'`
-max=6
+max=7
 swapTrigger=20
-swapTriggerDown=2
+swapTriggerDown=4
 
 while true
 do
@@ -9,16 +9,11 @@ do
         test=`free --giga | awk 'NR==2 {print $4}'`
         if (($test < $max))
         then
-                echo $test
                 swapon /dev/nvme0n1p3
-        else
-                echo no $test
         fi
 
         test2=`free --giga | awk 'NR==3 {print $3}'`
-        echo
-        echo $test
-        echo $test2
+
         if (($test2 > $swapTrigger))
         then
                 swapon /dev/sda
@@ -26,12 +21,9 @@ do
                 swapoff /dev/sda
         fi
 
-        if (($test > $max))
+        if (($test > $max)) && (($test2 < $swapTriggerDown))
         then
-                if (($test2 < $swapTriggerDown))
-                then
-                        swapoff /dev/nvme0n1p3
-                fi
+                swapoff /dev/nvme0n1p3
         fi
 
 done
