da1532084a8702f52509cea7ab1be5d2032fe215
joyel24 <joel.martin.2104@gmail.com>
2025-03-26 02:52:26 +0100

    Update comments

--- dynamicSwap.sh
+++ dynamicSwap.sh
@@ -1,4 +1,4 @@
-minFreeRAM=7                #Miniimum free GB RAM before enabling swap
+minFreeRAM=7                #Minimum free GB RAM before enabling swap
 swapTriggerUp=20            #Max swap GB used before enabling secondary swap partition
 swapTriggerDown=4           #Do not disable 1st swap partition (nvme0n1p3 in my case) if swap GB used is > than this variable
 swap1=/dev/nvme0n1p3        #Prioritqry swap file or partition (Faster one, nvme ssd in my case)
@@ -10,21 +10,21 @@ do
         
         freeRAM=`free --giga | awk 'NR==2 {print $4}'` #Get free RAM memory in GB
         if (($freeRAM < $minFreeRAM))
-        then
+        then                                 #if low free RAM, enable 1st swap
                 swapon $swap1
         fi
 
         usedSWAP=`free --giga | awk 'NR==3 {print $3}'` #Get swap usage in GB
 
         if (($usedSWAP > $swapTriggerUp))
-        then
+        then                                #enable 2nd swap if swap usage hit $swapTriggerUp
                 swapon $swap2
-        else
+        else                                #Disable 2nd swap if useless
                 swapoff $swap2
         fi
 
         if (($freeRAM > $minFreeRAM)) && (($usedSWAP < $swapTriggerDown))
-        then
+        then                                #Disable swap if free GB ram is > $minFreeRAM and if swap GB used is < $swapTriggerDown
                 swapoff $swap1
         fi
 
